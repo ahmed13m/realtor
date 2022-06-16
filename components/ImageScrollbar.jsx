9088a062b2b5ab3cd35e390112d1a9da23038552
ابن جاويد احمد <musheab658@gmail.com>
2022-06-16 21:24:59 +0530

    first commit

--- components/ImageScrollbar.jsx
+++ components/ImageScrollbar.jsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { Box, Icon, Flex } from "@chakra-ui/react";
 import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
+// import Item from "framer-motion/types/components/Reorder/Item";
 
 const LeftArrow = () => {
 	const { scrollPrev } = useContext(VisibilityContext);
@@ -43,11 +44,12 @@ export default function ImageSrollbar({ data }) {
 			style={{ overflow: "hidden" }}
 		>
 			{data.map((item) => (
-				<Box width="910px" itemId={item.id} overflow="hidden" p="1">
+				<Box width="910px" itemId={item.id} overflow="hidden" p="1" key={item}>
 					<Image
 						placeholder="blur"
 						blurDataURL={item.url}
 						src={item.url}
+						alt="ss"
 						width={1000}
 						height={500}
 						sizes="(max-width: 500px) 100px, (max-width: 1023px) 400px, 1000px"
